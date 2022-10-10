62d03dbf78fc6ec97e578d7f5b13a2276edab3da
galiyev <kairat.galiyev@gmail.com>
2022-10-10 21:22:30 +0600

    dsa

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,6 +9,9 @@ public class AppUser
     public int Id { get; set; }
     
     public string? UserName { get; set; }
+
+    public byte[] PasswordHash { get; set; }
     
-    
+    public byte[] PasswordSalt { get; set; }
+
 }
