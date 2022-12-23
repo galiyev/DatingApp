07a573b038821bdbf0a411e04005aeb216b77209
galiyev <kairat.galiyev@gmail.com>
2022-12-23 10:58:54 +0600

    Удаление currentUser$ из Nav

--- client2/src/app/nav/nav.component.ts
+++ client2/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AccountService} from "../_services/account.service";
+import {Observable, of} from "rxjs";
+import {User} from "../_model/user";
 
 @Component({
   selector: 'app-nav',
@@ -9,22 +11,24 @@ import {AccountService} from "../_services/account.service";
 export class NavComponent implements OnInit {
   model:any ={}
   loggedIn = false;
+  currentUser$:Observable<User | null> = of(null);
 
   constructor(private accountService:AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
+  // избавляемся от вызова этого метода
   getCurrentUser(){
     this.accountService.currentUser$.subscribe({
         next: user => this.loggedIn = !!user,
        error: error => console.log(error)
     })
   }
+
   logout(){
     this.accountService.logout();
-    this.loggedIn = false;
   }
 
   login(){
