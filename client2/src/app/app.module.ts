4acc9b40486ecfda41f1d6f969b79fcb8a956664
galiyev <kairat.galiyev@gmail.com>
2022-12-25 01:24:39 +0600

    End of 6

--- client2/src/app/app.module.ts
+++ client2/src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {NgIf} from "@angular/common";
-import {BsDropdownModule} from "ngx-bootstrap/dropdown";
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailsComponent } from './members/member-details/member-details.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {Toast, ToastrModule } from 'ngx-toastr';
+import {SharedModule} from "./_modules/shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -35,11 +32,7 @@ import {Toast, ToastrModule } from 'ngx-toastr';
     BrowserModule,
     AppRoutingModule,
     NgIf,
-    BsDropdownModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot({
-      positionClass:"toast-bottom-right"
-    }), // ToastrModule added
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
