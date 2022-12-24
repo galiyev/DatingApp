c81fc7e8f4436240d5c2cfa5a191296d5b0edad6
galiyev <kairat.galiyev@gmail.com>
2022-12-24 23:34:54 +0600

    sas

---
+++ client2/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AccountService} from "../_services/account.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  // @Input() usersFromHomeComponent:any;
+  @Output() cancelRegister = new EventEmitter<boolean>();
+  model: any = {};
+  constructor(private accountService:AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register(){
+     this.accountService.register(this.model).subscribe({
+       next: () => {
+         // console.log(response);
+         this.cancel();
+       },
+       error: error => console.log(error)
+     })
+  }
+
+
+  cancel(){
+    console.log('canceled');
+    this.cancelRegister.emit(false);
+  }
+
+}
